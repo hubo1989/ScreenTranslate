3cb9c852bc71680366db592fbd41d78c2a5fdd74
serdar-mac-mini-server <serdar-mac-mini-server@192.168.1.7>
2026-01-04 10:19:13 +0300

    Initial commit from Specify template

---
+++ edashot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  edashot
+//
+//  Created by serdar-mac-mini-server on 4.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ edashot/edashotApp.swift
@@ -0,0 +1,17 @@
+//
+//  edashotApp.swift
+//  edashot
+//
+//  Created by serdar-mac-mini-server on 4.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct edashotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
